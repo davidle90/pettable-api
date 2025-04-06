c9e374e990733bc08aa1bc383507649e6db576a5
davidle90 <89838376+davidle90@users.noreply.github.com>
2025-04-06 10:37:02 +0200

    feat(pets): update pet routes to make index and show public

--- routes/api_v1.php
+++ routes/api_v1.php
@@ -4,10 +4,13 @@ use App\Http\Controllers\Api\V1\PetController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\V1\UserController;
 
+Route::get('pets', [PetController::class, 'index']);
+Route::get('pets/{reference_id}', [PetController::class, 'show']);
+
 Route::middleware('auth:sanctum')->group(function () {
     Route::apiResource('users', UserController::class)->except(['update']);
     Route::patch('users/{user}', [UserController::class, 'update']);
 
-    Route::apiResource('pets', PetController::class)->except(['update']);
+    Route::apiResource('pets', PetController::class)->except(['update', 'show', 'index']);
     Route::patch('pets/{pet}', [PetController::class, 'update']);
 });
